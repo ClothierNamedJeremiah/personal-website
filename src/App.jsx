eb4cac32b8c02cb8ff02340b810e732faa254906
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-03-06 17:21:56 -0800

    inital project commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+const App = () => (
+  <div>
+    React App
+  </div>
+);
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const REGEX_JS = /\.(js|jsx)$/;
+const REGEX_LESS = /\.less$/;
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: REGEX_JS,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: REGEX_LESS,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  devServer: {
+    open: 'Google Chrome',
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hot: true,
+  },
+};
