ee156a35d27c0eac7eda96407599d1743f63bca4
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-03-07 22:57:22 -0800

    footer is more explicit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-import About from './components/About';
-import Contactbar from './components/Contactbar';
-import Socialbar from './components/Socialbar';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Footer from './components/Footer';
 
 const App = () => (
   <div className="app">
     <Navbar />
     <About />
-    <About />
-    <Socialbar />
-    <Contactbar />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Contactbar from './Contactbar';
+import Socialbar from './Socialbar';
+
+import '../styles/footer.less';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Socialbar />
+      <Contactbar />
+    </footer>
+  );
+}
+
+export default Footer;
