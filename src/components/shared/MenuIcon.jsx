fbcd38a29d70af4aa90fa1caaf381f7344847eb6
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-04-18 21:54:07 -0700

    state relies on previous

--- src/components/shared/MenuIcon.jsx
+++ src/components/shared/MenuIcon.jsx
@@ -8,7 +8,7 @@ const MenuIcon = (props) => {
     <div className="menu-wrapper">
       <div
         className={isMenuOpen ? "menu-icon active" : "menu-icon"}
-        onClick={() => setMenuOpen(!isMenuOpen)}
+        onClick={(prevState) => setMenuOpen(!prevState)}
         role="menuitem"
         tabIndex={-1}
       >
