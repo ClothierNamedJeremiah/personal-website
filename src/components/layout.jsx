50dda7f8466666560d6b31e0d147b9bb50e9010c
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-04-28 22:45:23 -0700

    adding basic SEO meta tags

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,10 +8,25 @@ import NavBar from './NavBar';
 const Layout = ({ children }) => (
   <>
     <Head>
-      <meta charSet="utf-8" />
       <title>Jeremiah Clothier</title>
 
+      {/* metadata */}
+      <meta charSet="utf-8" />
+      <meta name="description" content="Jeremiah Clothier's personal website and porfolio" />
+      <meta name="theme-color" content="#303c6c" />
+      <meta name="keywords" content="Jeremiah Clothier, Software Engineer, Personal Website" />
+      <meta name="author" content="Jeremiah Clothier" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Jeremiah Clothier" />
+      <meta property="og:description" content="Jeremiah Clothier's personal website and porfolio" />
+      <meta property="og:site_name" content="Personal Website" />
+
+      {/* icons */}
       <link rel="icon" href="/favicon-32x32.png" />
+      <link rel="shortcut icon" href="/favicon-32x32.png" />
+      <link rel="apple-touch-icon" href="/favicon-32x32.png" />
+
+      {/* CDN Content */}
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
     </Head>
