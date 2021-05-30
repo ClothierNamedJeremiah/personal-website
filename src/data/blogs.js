5d2ee161b6e538dbdcf7b59e88a91118d1ba5a02
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2021-05-29 21:07:47 -0700

    posted new blog

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,10 +1,17 @@
 const BLOGS = [
   {
+    title: 'How to easily hide a HTML element',
+    shortDescription: 'There are three easy ways to hide an element using CSS; each of them a little different than the other.',
+    datePublished: '05-29-2021',
+    estimatedTimeToRead: '3 min read',
+    blogPostUrl: 'https://clothiernamedjeremiah.medium.com/how-to-easily-hide-an-html-element-9105aee25612?sk=fe6471df1d95b1bad809c3251ac170a9',
+  },
+  {
     title: 'A simple solution to optimize React re-renders',
     shortDescription: 'Without using: React.memo, useMemo, PureComponent, or shouldComponentUpdate.',
     datePublished: '05-05-2021',
     estimatedTimeToRead: '5 min read',
-    blogPostUrl: 'https://clothiernamedjeremiah.medium.com/a-simple-solution-to-optimize-react-re-renders-3fce6713ccc8?sk=86e936de9fb90f62912c7f20ed2d7231'
+    blogPostUrl: 'https://clothiernamedjeremiah.medium.com/a-simple-solution-to-optimize-react-re-renders-3fce6713ccc8?sk=86e936de9fb90f62912c7f20ed2d7231',
   },
   {
     title: "Understanding the CSS 'position' property",
@@ -14,7 +21,7 @@ const BLOGS = [
     blogPostUrl: 'https://clothiernamedjeremiah.medium.com/how-to-position-html-elements-properly-using-css-3fc559f6e5ff',
   },
   {
-    title: 'Creating a React Application from Scratch',
+    title: 'Creating a react application from scratch',
     shortDescription: 'Learn how to create your own react application with Babel, Webpack, and Hot Module Replacement.',
     datePublished: '04-05-2021',
     estimatedTimeToRead: '7 min read',
