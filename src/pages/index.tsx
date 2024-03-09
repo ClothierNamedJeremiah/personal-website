05a2d3801cc5564ce4efaa9e3123efab407ce3be
Jeremiah Clothier <clothiernamedjeremiah@gmail.com>
2024-03-08 22:49:31 -0800

    feat: remove the skills section
    I don't really like this and having to maintain all the images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import Welcome from 'components/Welcome';
-import Skills from 'components/Skills';
-
-import styles from './Home.module.css';
 
 const Home = () => (
-  <div className={`${styles.container} m-4`}>
+  <div className="m-4">
     <Welcome />
-    <Skills />
   </div>
 );
 
